ffa2787120fcfd65fe4ab607794e4958cc4b7d0c
salesAndoffers <salesandoffers.co@gmail.com>
2025-10-24 10:45:14 +0300

    Fix: Make NavSpacer conditionally render when navbar is hidden on admin pages

--- sales_offers_frontend/components/NavSpacer.tsx
+++ sales_offers_frontend/components/NavSpacer.tsx
@@ -1,11 +1,17 @@
 "use client";
 
 import { useNavbar } from "../contexts/NavbarContext";
+import { usePathname } from "next/navigation";
 
 export default function NavSpacer() {
   const { navHidden } = useNavbar();
+  const pathname = usePathname();
+  const isAdminPage = pathname?.startsWith('/admin');
   
-  return (
-    <div className={`transition-all duration-300 ${navHidden ? 'h-0' : 'h-16'}`}></div>
-  );
+  // Don't render spacer if on admin page and navbar is hidden
+  if (isAdminPage && navHidden) {
+    return null;
+  }
+  
+  return <div className="h-16"></div>;
 }
