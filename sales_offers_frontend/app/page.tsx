bbc1a744ce740dc3ff394503078608da11c9496d
salesandoffersco-sudo <salesandoffers.co@gmail.com>
2025-11-26 10:15:51 +0300

    Reduce background blur to 5px

--- sales_offers_frontend/app/page.tsx
+++ sales_offers_frontend/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
         <div className="absolute bottom-40 right-1/3 w-24 h-24 border-2 border-blue-500/25 dark:border-blue-400/15 rounded-full animate-ping" style={{ animationDuration: '3s' }}></div>
 
         {/* iOS/MacOS Blur Overlay */}
-        <div className="absolute inset-0 bg-white/10 dark:bg-black/10" style={{ backdropFilter: 'blur(20px)', WebkitBackdropFilter: 'blur(20px)' }}></div>
+        <div className="absolute inset-0 bg-white/10 dark:bg-black/10" style={{ backdropFilter: 'blur(5px)', WebkitBackdropFilter: 'blur(5px)' }}></div>
       </div>
 
       {/* Content */}
